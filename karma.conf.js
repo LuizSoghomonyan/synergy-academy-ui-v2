62ef779f94cf4931d5d1e717a88b76824fefe425
Arthur Yeganyan <yeganyanarthur@gmail.com>
2022-01-28 15:18:00 +0400

    initial commit

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/synergy-academy-ui-v2'),
+      dir: require('path').join(__dirname, './coverage/synergy-academy-ui'),
       subdir: '.',
       reporters: [
         { type: 'html' },
